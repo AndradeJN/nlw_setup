8163cc67d3a0f48bd6ba19808b9a10f5adb52ac7
Jose Andrade <josenetogui@hotmail.com>
2023-04-16 10:07:50 -0300

    Back end

---
+++ server/src/server.ts
@@ -0,0 +1,26 @@
+// Back end API RESTful
+
+import Fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+
+const app = Fastify()
+const prisma = new PrismaClient()
+app.register(cors)
+
+app.get('/', async () => {
+    const habits = await prisma.habit.deleteMany({
+        where: {
+            title:{
+                startsWith: 'Beber'
+            }
+        }
+    })
+})
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('HTTP Server On!')
+})
+
